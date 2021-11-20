ba7affe1961dd7eea937bc0423b72b7a65e141aa
Cristian <cristianoteroadsi@gmail.com>
2021-11-20 10:06:03 -0500

    Particles Done
    Need some extra details but is done

--- public/js/home.particles.js
+++ public/js/home.particles.js
@@ -1,15 +1,13 @@
 const canvas = document.getElementById("canvas-home");
 const ctx = canvas.getContext("2d");
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+let particleArray;
 
-let particlesArray;
-
-// Get Mouse position
 let mouse = {
   x: null,
   y: null,
-  radius: (canvas.height / 80) * (canvas.width / 80),
+  radius: (canvas.height / 100) * (canvas.width / 100),
 };
 
 window.addEventListener("mousemove", function (event) {
@@ -17,7 +15,6 @@ window.addEventListener("mousemove", function (event) {
   mouse.y = event.y;
 });
 
-// particles
 class Particle {
   constructor(x, y, directionX, directionY, size, color) {
     this.x = x;
@@ -28,82 +25,104 @@ class Particle {
     this.color = color;
   }
 
-  // Draw individual particle
   draw() {
     ctx.beginPath();
     ctx.arc(this.x, this.y, this.size, 0, Math.PI * 2, false);
-    ctx.fillStyle = "white";
+    // ircles 
+    ctx.fillStyle = "#24214b";
     ctx.fill();
   }
 
   update() {
-    // Check the particles sitll within canvas [it bounce to in when touch the edge of canvas]
     if (this.x > canvas.width || this.x < 0) {
       this.directionX = -this.directionX;
     }
-    if (this.y > canvas.height || this.y < 0) {
+    if (this.y + this.size > canvas.height || this.y - this.size < 0) {
       this.directionY = -this.directionY;
     }
 
-    // calculate the radius distance betwen mouse and particles
-    let dx = mouse.x - this.x; // [A hick]
-    let dy = mouse.y - this.y; // [B hick]
-    distance = Math.sqrt(dx * dx + dy * dy); //[Square Root ]
+    let dx = mouse.x - this.x;
+    let dy = mouse.y - this.y;
+    let distance = Math.sqrt(dx * dx + dy * dy);
 
-    // if the distances is less than mouse radius asigned we have collission
     if (distance < mouse.radius + this.size) {
-      // if the (mouse is less o more that particle ) AND (particle.X is far of edge)
-      if (mouse.x < this.x && this.x < canvas.width - this.size*10) {
-        // push the partcle to wrong direction
+      if (mouse.x < this.x && this.x < canvas.width - this.size * 10) {
         this.x += 10;
       }
       if (mouse.x > this.x && this.x > this.size * 10) {
         this.x -= 10;
       }
-
       if (mouse.y < this.y && this.y < canvas.height - this.size * 10) {
-        this.y += 0;
+        this.y += 10;
       }
       if (mouse.y > this.y && this.y > this.size * 10) {
         this.y -= 10;
       }
     }
-    //move paritcle [it folows the mouse directions]
+
     this.x += this.directionX;
     this.y += this.directionY;
-    // TODO: HAsta aqui revisado verificar de nuevo  el draw
     this.draw();
   }
 }
 
 function init() {
-  particlesArray = [];
-
-  let numberParticles = (canvas.height*canvas.width)/ 9000;
-
+  particleArray = [];
+  let numberParticles = (canvas.height * canvas.width) / 9000;
   for (let i = 0; i < numberParticles; i++) {
     let size = Math.random() * 5 + 1;
-
     let x = Math.random() * (innerWidth - size * 2 - size * 2) + size * 2;
     let y = Math.random() * (innerHeight - size * 2 - size * 2) + size * 2;
-
     let directionX = Math.random() * 5 - 2.5;
     let directionY = Math.random() * 5 - 2.5;
-    color = "#fffff";
 
-    particlesArray.push(
-      new Particle(x, y, directionX, directionY, size, color)
-    );
+    let color = "#24214b";
+    particleArray.push(new Particle(x, y, directionX, directionY, size, color));
   }
 }
 
 function animate() {
   requestAnimationFrame(animate);
   ctx.clearRect(0, 0, innerWidth, innerHeight);
-  for (let i = 0; i < particlesArray.lenght; i++) {
-    particlesArray[i].update();
+
+  for (let i = 0; i < particleArray.length; i++) {
+    particleArray[i].update();
+  }
+  connect();
+}
+
+function connect() {
+  
+  for (let a = 0; a < particleArray.length; a++) {
+    for (let b = a; b < particleArray.length; b++) {
+      let distance =
+        (particleArray[a].x - particleArray[b].x) * (particleArray[a].x - particleArray[b].x) +
+        (particleArray[a].y - particleArray[b].y) * (particleArray[a].y - particleArray[b].y);
+
+      if (distance < (canvas.width / 7) * (canvas.height / 7)) {
+        let opacity =  .5 - (distance/20000);
+        // ctx.strokeStyle = 'rgba(81, 74, 157,.08)';
+        ctx.strokeStyle = 'rgba(81, 74, 157,'+opacity+')';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(particleArray[a].x, particleArray[a].y);
+        ctx.lineTo(particleArray[b].x, particleArray[b].y);
+        ctx.stroke();
+      }
+    }
   }
 }
+
+window.addEventListener("resize", function () {
+  canvas.width = innerWidth;
+  canvas.height = innerHeight;
+  mouse.radius = (canvas.height / 80) * (canvas.height / 80);
+});
+
+window.addEventListener("mouseOut", function (event) {
+  mouse.x = undefined;
+  mouse.y = undefined;
+});
 
 init();
 animate();
