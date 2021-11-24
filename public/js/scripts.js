d9cf2d1486f3307291e0556004d1c97ade19ad16
Cristian <cristianoteroadsi@gmail.com>
2021-11-23 21:29:49 -0500

    Light Mode in progress

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,7 +1,40 @@
+// Modal variables
 let btn_quickMessage = document.querySelector(".btn-messagge");
 let contenModal = document.querySelector(".modal-container");
 let modal = document.querySelector(".modal");
 let btn_colseModal = document.querySelector(".close-modal-btn");
+
+//Dark $ Light Mode
+let mode = document.querySelector("#switch-mode");
+let modeState = 0;
+let rootElement = document.querySelector(":root");
+
+function changeMode() {
+  mode.classList.toggle("active");
+  if (modeState == 1) {
+    lightMode()
+  }else if (modeState == 0) {
+    darkMode()
+  } 
+}
+
+function darkMode() {
+  rootElement.style.setProperty("--main-bg-dark", "#0c1024");
+}
+function lightMode() {
+  rootElement.style.setProperty("--main-bg-dark", "#ffffff");
+  rootElement.style.setProperty("--main-bg-dark", "#ffffff");
+}
+
+mode.addEventListener("click", function (e) {
+  if (modeState == 0) {
+    modeState = 1;
+  } else if (modeState) {
+    modeState = 0;
+  }
+  changeMode();
+  e.preventDefault();
+});
 
 function colseModal() {
   modal.classList.remove("show-modal");
@@ -14,6 +47,8 @@ function openModal() {
   modal.classList.add("show-modal");
 }
 
+function name(params) {}
+
 btn_quickMessage.addEventListener("click", function (e) {
   openModal();
   e.preventDefault();
@@ -25,7 +60,6 @@ btn_colseModal.addEventListener("click", function (e) {
 });
 
 window.addEventListener("click", function (e) {
-  console.log(e.target);
   if (e.target == contenModal) {
     colseModal();
   }
